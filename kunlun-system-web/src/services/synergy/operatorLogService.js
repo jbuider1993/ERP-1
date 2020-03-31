565f5ef54345f5676e487ad23ddeb9e871b264ce
野性心态 <sjxue126@126.com>
2020-03-31 11:06:51 +0800

    删除文件 kunlun-system-web

--- kunlun-system-web/src/services/synergy/operatorLogService.js
+++
@@ -1,6 +0,0 @@
-import * as request from '../../utils/request';
-import config from '../../config/config';
-
-export function getLogList(params) {
-  return request.get(`${config.system_api.getLogList}`, params);
-}
--- kunlun-system-web/src/services/user/onlineService.js
+++
@@ -1,6 +0,0 @@
-import * as request from '../../utils/request';
-import config from '../../config/config';
-
-export function getAllOnlineUser(params) {
-  return request.get(`${config.system_api.getOnlineUserList}`, params);
-}
